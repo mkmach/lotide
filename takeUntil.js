1117c30278e07283aec3fd09d6da1c177c9e949b
mkmach <mkmach@hotmail.com>
2022-11-24 14:58:46 -0800

    creates a slice of array until cb is falsey

---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const eqArrays = function(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅  Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+//Creates a slice of array with elements taken from the beginning. Elements are taken until callback returns falsey. The callback is invoked with three arguments: (value, index, array).
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      break;
+    }
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
+
